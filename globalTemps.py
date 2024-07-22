273a93beb4c0b7e6170e47f3e8388dfd6aea5160
dankpad <dgreen02@uab.edu>
2024-07-22 15:11:23 -0500

    June 22 updates

--- globalTemps.py
+++ globalTemps.py
@@ -1,9 +1,13 @@
 import json
+import urllib.request
 
-jsonData = 'data.json'
 
-with open(jsonData, encoding='utf-8') as data:
-    anomalies = json.load(data)
+jsonData = 'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/land_ocean/1/10/1880-2022.json'
+
+#with open(jsonData, encoding='utf-8') as data:
+with urllib.request.urlopen(jsonData) as json_stream:
+    data = json_stream.read().decode('utf-8')
+    anomalies = json.loads(data)
 
 print(anomalies['description'])
 
