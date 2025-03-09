bf11c3b8b537110a4020dacc0809e3eb43aa8825
Darby-Green <dgreen02@uab.edu>
2025-03-09 16:16:13 -0500

    Mar 9 push

--- Section_11/tkinterDemo.py
+++ Section_11/tkinterDemo.py
@@ -1,10 +1,28 @@
 import tkinter
 
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
 
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello there")
 mainWindow.geometry('640x480')
+
+label = tkinter.Label(mainWindow, text="Hello world")
+label.pack(side='top')
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.pack(side='left', anchor='n')
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.pack(side='right', anchor='n', fill=tkinter.Y, expand=True)
+
+button1 = tkinter.Button(rightFrame, text='button1')
+button1.pack(side='top')
+button2 = tkinter.Button(rightFrame, text='button2')
+button2.pack(side='top')
+button3 = tkinter.Button(rightFrame, text='button3')
+button3.pack(side='top')
+
 mainWindow.mainloop()
