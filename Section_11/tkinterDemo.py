90382f88b0702afaa022cd8b74eb83c2c2487cf4
Darby-Green <dgreen02@uab.edu>
2025-04-18 15:00:21 -0500

    4/18 push

--- Section_11/tkinterDemo.py
+++ Section_11/tkinterDemo.py
@@ -4,25 +4,29 @@ import tkinter
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello there")
-mainWindow.geometry('640x480')
+mainWindow.geometry('640x480+8+200')
 
 label = tkinter.Label(mainWindow, text="Hello world")
-label.pack(side='top')
+label.grid(row=0, column=0)
 
 leftFrame = tkinter.Frame(mainWindow)
-leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+leftFrame.grid(row=1, column=1)
 
 canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
-canvas.pack(side='left', anchor='n')
+canvas.grid(row=1, column=0)
 
 rightFrame = tkinter.Frame(mainWindow)
-rightFrame.pack(side='right', anchor='n', fill=tkinter.Y, expand=True)
-
+rightFrame.grid(row=1, column=2, sticky='n')
 button1 = tkinter.Button(rightFrame, text='button1')
-button1.pack(side='top')
+button1.grid(row=0, column=0)
 button2 = tkinter.Button(rightFrame, text='button2')
-button2.pack(side='top')
+button2.grid(row=1, column=0)
 button3 = tkinter.Button(rightFrame, text='button3')
-button3.pack(side='top')
+button3.grid(row=2, column=0)
+
+# configure the columns
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
 
 mainWindow.mainloop()
