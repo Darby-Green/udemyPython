69def79848a7b9d8ece00f3803c6380880d43804
Darby-Green <dgreen02@uab.edu>
2025-08-05 15:03:26 -0500

    Aug 5 commit

--- Section_12/game.py
+++ Section_12/game.py
@@ -1,4 +1,5 @@
 from player import Player
+from enemy import Enemy, Troll, Vampyre
 
 darby = Player("Darby")
 
@@ -9,3 +10,27 @@ print(basicEnemy)
 
 basicEnemy.take_damage(4)
 print(basicEnemy)
+
+uglyTroll = Troll("Pug")
+print("Ugly Troll - {}".format(uglyTroll))
+
+anoutherTroll = Troll("Ug")
+print("Anouther troll - {}".format(anoutherTroll))
+anoutherTroll.take_damage(12)
+print(anoutherTroll)
+
+brotherTroll = Troll("Urg")
+print(brotherTroll)
+
+uglyTroll.grunt()
+anoutherTroll.grunt()
+brotherTroll.grunt()
+
+vamp = Vampyre("Dracula")
+print(vamp)
+vamp.take_damage(3)
+print(vamp)
+
+while vamp.alive:
+    vamp.take_damage(1)
+    print(vamp)
